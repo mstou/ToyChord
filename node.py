309d8be3b5902fc6b1d201800c62706acd7c7582
Miltiadis Stouras <m.stouras@gmail.com>
2021-02-25 14:24:51 +0200

    Introduce constants.py & Handle bad join requests
    Co-authored-by: Chariton Charitonidis <haritonch@yahoo.com>
    Co-authored-by: Iasonas Nikolaou <iasonasnikolaou11@hotmail.com>

--- node.py
+++ node.py
@@ -1,7 +1,9 @@
 import sys
 import flask
-from hashlib import sha1
+from lib.constants import *
 from lib.get_ip import get_ip
+from flask import abort, request
+from lib.id_utils import create_id
 
 my_ip = get_ip()
 port  = sys.argv[1]
@@ -9,16 +11,28 @@ port  = sys.argv[1]
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 
-my_key       = sha1(f'{my_ip}:{port}'.encode('utf-8'))
-previous_key = None
-next_key     = None
+my_id       = create_id(my_ip, port)
+previous_id = None
+next_id     = None
 
 '''
 Endpoint that a node hits if it wants to join
 the system.
+
+Parameters:
+    ip  : The ip of the remote machine that wants to join the network
+    port: The port that the remote machine will use to answer queries
+
 '''
 @app.route('/join', methods=['POST'])
 def request_to_join():
-    raise NotImplementError
+    if (PORT not in request.args) or (IP not in request.args):
+        abort(BAD_REQUEST)
+
+    req_port = request.args.get(PORT)
+    req_ip   = request.args.get(IP)
+
+    new_key = create_id(req_ip, req_port)
+
 
 app.run(port=int(port))
