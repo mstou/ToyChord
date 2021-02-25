921ebea524e6a09f065798453e99343c5d80bbd7
Miltiadis Stouras <m.stouras@gmail.com>
2021-02-25 22:28:50 +0200

    Implement is_id_in_range
    Co-authored-by: Chariton Charitonidis <haritonch@yahoo.com>
    Co-authored-by: Iasonas Nikolaou <iasonasnikolaou11@hotmail.com>

--- lib/id_utils.py
+++ lib/id_utils.py
@@ -4,4 +4,18 @@ def create_id(ip, port):
     return sha1(f'{ip}:{port}'.encode('utf-8'))
 
 def is_id_in_range(target, start, end):
-    raise NotImplementedError
+
+    targetInt  = int(target.hexdigest(), 16)
+    startInt   = int(start.hexdigest(), 16)
+    endInt     = int(end.hexdigest(), 16)
+
+    if targetInt > startInt and targetInt < endInt:
+        return True
+
+    if startInt > endInt and targetInt < endInt:
+        return True
+
+    if startInt > endInt and targetInt > startInt:
+        return True
+
+    return False
