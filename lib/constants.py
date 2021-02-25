fbdede1e649cbf639770d58c5f364ca19a58a765
Miltiadis Stouras <m.stouras@gmail.com>
2021-02-25 22:28:09 +0200

    Add some functions to make common requests and update constants
    Co-authored-by: Chariton Charitonidis <haritonch@yahoo.com>
    Co-authored-by: Iasonas Nikolaou <iasonasnikolaou11@hotmail.com>

---
+++ lib/constants.py
@@ -0,0 +1,11 @@
+PORT = 'port'
+IP   = 'ip'
+HTTP = 'http://'
+CUR_PREVIOUS = 'current_previous'
+NEXT_IP   = 'next_ip'
+NEXT_PORT = 'next_port'
+PREV_IP   = 'prev_ip'
+PREV_PORT = 'prev_port'
+
+BAD_REQUEST  = 400
+UNAUTHORIZED = 401
---
+++ lib/request_utils.py
@@ -0,0 +1,18 @@
+import requests
+from lib.constants import *
+
+def join_request(targetNode, port, ip):
+    url = HTTP + targetNode.get_address() + '/join'
+    params = f'{PORT}={port}&{IP}={ip}'
+    requests.get(url + '?' + params)
+
+def update_previous_request(targetNode, port, ip, cur_previous):
+    url = HTTP + targetNode.get_address() + '/update_previous'
+    params = f'{PORT}={port}&{IP}={ip}&{CUR_PREVIOUS}={cur_previous}'
+    requests.get(url + '?' + params)
+
+def join_successful_request(target, previous, next):
+    url = HTTP + target.get_address() + '/join_successful'
+    params = f'{NEXT_PORT}={next.get_port()}&{NEXT_IP}={next.get_ip()}' +\
+             f'{PREV_PORT}={previous.get_port()}&{PREV_IP}={previous.get_ip()}'
+    requests.get(url + '?' + params)
